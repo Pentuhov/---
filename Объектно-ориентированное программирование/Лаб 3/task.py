66b774ed67b345c5d2fb2d09939ece5d4065a771
Pentuhov <keys.marshal@yandex.ru>
2022-10-23 21:18:25 +0300

    1
    1

--- Объектно-ориентированное программирование/Лаб 3/task.py
+++ Объектно-ориентированное программирование/Лаб 3/task.py
@@ -14,45 +14,55 @@
 Отправьте одного из солдат первого героя следовать за ним. Выведите на экран
 идентификационные номера этих двух юнитов.
 """
-
-from random import randint
-
-
-class Hero:
-
-    def __init__(self, team):
-        self.team = team
-        self.id = randint(1, 100000)
-        self.lvl = 1
-
-    def lvlup(self):
-        self.lvl += 1
+import random
 
 
-class Soldier(Hero):
-    def follow_hero(self, unit):
-        if isinstance(unit, Hero):
-            return f'Солдат  команды {self.team} c номером {self.id}  идет за героем {unit.id}'
+class Unit:
+    def __init__(self, number, commandid):
+        self.number = number
+        self.commandId = commandid
 
 
-def create_teams():
-    radiant_hero = Hero(1)
-    dire_hero = Hero(2)
-    radiant_squad = []
-    dire_squad = []
-    for i in range(100):
-        unit = Soldier(randint(1, 2))
-        if(unit.team == 1):
-            radiant_squad.append(unit)
-        else:
-            dire_squad.append(unit)
-    if (len(radiant_squad) > len(dire_squad)):
-        radiant_hero.lvlup()
+class Hero(Unit):
+    def __init__(self,  number, commandid, name, level=1):  # При инициализации добавляем свойства name и level
+        Unit.__init__(self, number, commandid)
+        self.name = name  # Привязываем классу Hero свойство level и name
+        self.level = level
+
+    def getlevel(self):
+        return self.level
+
+    def inclevel(self):
+        self.level += 1
+        print('Уровень героя', self.name,'увеличен на 1 и равен', self.level)
+
+
+class Soldier(Unit):
+    def gotohero(self, Hero):
+        print('Солдат номер', self.number, 'следует за героем', Hero.name, 'с номером', Hero.number)
+
+
+H1 = Hero(1, 1, 'Герой 1')  # Создаем героев с номерами 1 и 2
+H2 = Hero(2, 2, 'Герой 2')
+armyH1, armyH2 = [], []  # Списки солдат
+
+for i in range(3, 10):  # Генерим нечетное количество солдат
+    n = random.randint(0, 1)
+    if n:
+        armyH1.append(Soldier(i, 1))
+        print('Солдат с номером', i, 'добавлен в армию', H1.name)
     else:
-        dire_hero.lvlup()
-    print(radiant_squad[1].follow_hero(radiant_hero))
-    print(f'Id солдата -  {radiant_squad[1].id}\nId героя - {radiant_hero.id} Уровень героя {radiant_hero.lvl}')
+        armyH2.append(Soldier(i, 2))
+        print('Солдат с номером', i, 'добавлен в армию', H2.name)
 
-if __name__ == '__main__':
-    create_teams()
+print('Армия героя', H1.name, ':', len(armyH1))
+print('Армия героя', H2.name, ':', len(armyH2))
 
+if len(armyH1) > len(armyH2):
+    print('В армии', H1.name, 'больше солдат, увеличиваем его уровень')
+    H1.inclevel()
+else:
+    print('В армии', H2.name, 'больше солдат, увеличиваем его уровень')
+    H2.inclevel()
+
+armyH1[1].gotohero(H2)
